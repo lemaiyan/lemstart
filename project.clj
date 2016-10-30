7b04bbe90c5b8ec236906cf5c486fa50314aba9c
“lemaiyangit config --global user.email “george.kamunya@gmail.com <gkamunya@mo-de.com>
2016-10-30 22:24:33 +0300

    JL: Updated read me and updated compojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lemstart/lein-template "0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lemstart/lein-template "0.1"
+  :description "A Leiningen template for starting REST API web server running on httpkit and postgres database"
+  :url "https://github.com/lemaiyan/lemstart.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
