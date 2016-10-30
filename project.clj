f6b1ff030abee29b43adca6e9d4821d9265cf5bf
“lemaiyangit config --global user.email “george.kamunya@gmail.com <gkamunya@mo-de.com>
2016-10-30 22:45:02 +0300

    JL: Minor fixes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lemstart/lein-template "0.3"
+(defproject lemstart/lein-template "0.4"
   :description "A Leiningen template for starting REST API web server running on httpkit and postgres database"
   :url "https://github.com/lemaiyan/lemstart.git"
   :license {:name "Eclipse Public License"
--- resources/leiningen/new/lemstart/project.clj
+++ resources/leiningen/new/lemstart/project.clj
@@ -1,4 +1,4 @@
-(defproject {{name}}
+(defproject {{name}} "0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
