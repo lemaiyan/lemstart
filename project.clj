29e876fcd88dc9909aee67133365807ef1c79937
gkamunya <gkamunya@gkamunyas-MacBook-Pro.local>
2017-03-15 15:42:27 +0300

    removed the .hgignore file

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject lemstart/lein-template "1.2"
+(defproject lemstart/lein-template "1.4"
   :description "A Leiningen template for starting REST API web server running on httpkit and postgres database"
   :url "https://github.com/lemaiyan/lemstart.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories
+  [["clojars"
+    {:sign-releases false
+     :url "https://clojars.org/repo"}]])
